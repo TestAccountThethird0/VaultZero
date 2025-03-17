8d4596ac91eabfbad2e92713303d7abd42cac9db
TestAccountThethird0 <somerandomguythethird@gmail.com>
2025-03-17 13:23:28 +0200

    Update LoadBranches.lua

--- Branches/LoadBranches.lua
+++ Branches/LoadBranches.lua
@@ -1,1 +1,7 @@
-
+local Map = {
+    [142823291] = "https://raw.githubusercontent.com/TestAccountThethird0/VaultZero/refs/heads/main/Branches/MurderMystery2.lua",
+}
+--CODE--
+if table.find(Map,game.PlaceId) then
+    loadstring(Map[game.PlaceId])()
+end
